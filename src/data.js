85e8bcc3a4afd6a1edab75684ae1553e769c33b2
PrimoCrypt <104681010+PrimoCrypt@users.noreply.github.com>
2024-05-01 15:38:05 +0100

    Update: changed role form frontend to fullstack

--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@ export const profileData = {
     name: "Anesi O. Divine",
     image: "./assets/photo_2024-03-28_00-21-19.jpg",
     location: "Benin-city, Edo State, Nigeria.",
-    about: "\"Front-end Developer and Blockchain Advocate\"",
+    about: "\"FullStack  Developer and Blockchain Advocate\"",
     links: [
         {
             linkName: "GitHub",
